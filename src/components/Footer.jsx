8a43cbbd07e1dd64ec436ad9cffb66059c4b300d
mayaralharazi <malharaz@charlotte.edu>
2026-03-30 19:57:00 -0400

    l

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,42 @@ function Footer() {
     return (
       <footer>
         <p>
-          Designed by Mayar Alharazi |
-          <a href="https://github.com/mayaralharazi" target="_blank" rel="noreferrer"> GitHub</a> |
-          <a href="https://mayaralharazi.github.io/itis3135/" target="_blank" rel="noreferrer"> ITIS3135.io</a> |
-          <a href="#" target="_blank" rel="noreferrer"> ITIS3135@Vercel</a>
+          Page designed by{" "}
+          <a href="../mayaralharazistudio.com/">
+            Mayar Alharazi Studio
+          </a>{" "}
+          © 2026{" "}
+          <a href="https://github.com/mayaralharazi" target="_blank" rel="noreferrer">
+            GitHub
+          </a>{" "}
+          |{" "}
+          <a href="https://webpages.charlotte.edu/malharaz/" target="_blank" rel="noreferrer">
+            CLT Web
+          </a>{" "}
+          |{" "}
+          <a href="https://mayaralharazi.github.io/" target="_blank" rel="noreferrer">
+            GitHub.io
+          </a>{" "}
+          |{" "}
+          <a href="https://itis3135-react-phi.vercel.app/" target="_blank" rel="noreferrer">
+            ITIS3135@Vercel
+          </a>{" "}
+          |{" "}
+          <a href="https://www.freecodecamp.org/mayaralharazi" target="_blank" rel="noreferrer">
+            freeCodeCamp
+          </a>{" "}
+          |{" "}
+          <a href="https://www.linkedin.com/in/mayar-alharazi-64a385241/" target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://freecodecamp.org/certification/mayaralharazi/responsive-web-design-v9"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Certified in Responsive Web Design
+          </a>
         </p>
       </footer>
     );
