d830664637275d7d85557e1e630c60b1f95c75b4
mayaralharazi <malharaz@charlotte.edu>
2026-03-30 20:17:20 -0400

    ,

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -2,7 +2,33 @@ function Contract() {
     return (
       <main>
         <h2>Contract</h2>
-        <p>This is my contract page.</p>
+  
+        <p>
+          I, Mayar Alharazi agree to abide by the terms of the course contract in my Spring 2026,
+          ITIS 3135, Web App Design and Development with my instructor, Professor D.I. von Briesen.
+        </p>
+  
+        <p>
+          I understand that all work that I do on publicly available online tools will be available to the world,
+          and will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
+        </p>
+  
+        <p>
+          I also understand that it is my work that counts for attendance, not logins or showing up for class.
+          As such, failure to turn in assignments may show as absences.
+        </p>
+  
+        <p>
+          I also understand that given the structure and content of this class it’s possible to find many examples online
+          or even view my classmates’ code directly. I swear that I will only use these resources to learn, and will not cut
+          and paste code except where I have properly given credit (i.e. external libraries) and never from my classmates.
+        </p>
+  
+        <p>
+          <em>
+            Signed: Mayar Alharazi, 25/01/2026
+          </em>
+        </p>
       </main>
     );
   }
