929864ef71f46f5ea911c342400d6f6142738e2a
mayaralharazi <malharaz@charlotte.edu>
2026-03-30 20:37:45 -0400

    l

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,16 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav>
-      <div>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/contract">Contract</Link> |{" "}
-        <Link to="/survey">Survey</Link> |{" "}
-        <Link to="/cards">Cards</Link> |{" "}
-        <Link to="/inventory">Inventory</Link> |{" "}
-        <Link to="/documentation">Documentation</Link> |{" "}
-        <Link to="/product">Product</Link>
-      </div>
-
-      <div>
-        <Link to="/evaluations">CRAP Website Evaluations</Link> |{" "}
-        <Link to="/hobby">Hobby</Link>
-      </div>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/contract">Contract</Link> |{" "}
+      <Link to="/survey">Survey</Link> |{" "}
+      <Link to="/cards">Cards</Link> |{" "}
+      <Link to="/inventory">Inventory</Link> |{" "}
+      <Link to="/documentation">Documentation</Link> |{" "}
+      <Link to="/product">Product</Link>
+      <br />
+      <Link to="/evaluations">CRAP Website Evaluations</Link> |{" "}
+      <Link to="/hobby">Hobby</Link>
     </nav>
   );
 }
