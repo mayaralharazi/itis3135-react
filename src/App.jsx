4ece29f91acfcb47d6fd5a5eeac9a6f1e3b4a8a3
mayaralharazi <malharaz@charlotte.edu>
2026-04-21 21:26:29 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -14,6 +15,25 @@ import Evaluations from "./pages/Evaluations";
 import Hobby from "./pages/Hobby";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const titles = {
+      "/": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Home",
+      "/contract": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Contract",
+      "/fcc": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ FCC",
+      "/survey": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Survey",
+      "/cards": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Cards",
+      "/inventory": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Inventory",
+      "/documentation": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Documentation",
+      "/product": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Product",
+      "/evaluations": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Website Evaluations",
+      "/hobby": "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Hobby",
+    };
+
+    document.title = titles[location.pathname] || "Mayar Alharazi's Jolly Dolphin ~ ITIS 3135";
+  }, [location.pathname]);
+
   return (
     <>
       <header>
