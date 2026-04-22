1df3eb727606275e8eed1bf73d9d480792a83a46
mayaralharazi <malharaz@charlotte.edu>
2026-04-21 21:16:42 -0400

    Update FCC.jsx

--- src/pages/FCC.jsx
+++ src/pages/FCC.jsx
@@ -1,10 +1,16 @@
+import PageTitle from "../components/PageTitle";
+
 function FCC() {
-    return (
+  return (
+    <>
+      <PageTitle title="Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ FCC" />
+
       <main>
         <h2>FCC</h2>
         <p>This is my FCC page.</p>
       </main>
-    );
-  }
-  
-  export default FCC;
+    </>
+  );
+}
+
+export default FCC;
