f00881d6f6286e2cb0b28b3de2eca9830393eac3
mayaralharazi <malharaz@charlotte.edu>
2026-04-21 21:56:26 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,49 @@
 function Footer() {
-    return (
-      <footer>
-        <p>
-          Page designed by{" "}
-          <a href="../mayaralharazistudio.com/">
-            Mayar Alharazi Studio
-          </a>{" "}
-          © 2026{" "}
-          <a href="https://github.com/mayaralharazi" target="_blank" rel="noreferrer">
-            GitHub
-          </a>{" "}
-          |{" "}
-          <a href="https://webpages.charlotte.edu/malharaz/" target="_blank" rel="noreferrer">
-            CLT Web
-          </a>{" "}
-          |{" "}
-          <a href="https://mayaralharazi.github.io/" target="_blank" rel="noreferrer">
-            GitHub.io
-          </a>{" "}
-          |{" "}
-          <a href="https://itis3135-react-phi.vercel.app/" target="_blank" rel="noreferrer">
-            ITIS3135@Vercel
-          </a>{" "}
-          |{" "}
-          <a href="https://www.freecodecamp.org/mayaralharazi" target="_blank" rel="noreferrer">
-            freeCodeCamp
-          </a>{" "}
-          |{" "}
-          <a href="https://www.linkedin.com/in/mayar-alharazi-64a385241/" target="_blank" rel="noreferrer">
-            LinkedIn
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://freecodecamp.org/certification/mayaralharazi/responsive-web-design-v9"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Certified in Responsive Web Design
-          </a>
-        </p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer>
+      <p>
+        Page designed by{" "}
+        <a href="../mayaralharazistudio.com/">
+          Mayar Alharazi Studio
+        </a>{" "}
+        © 2026
+      </p>
+
+      <nav>
+        <a href="https://github.com/mayaralharazi" target="_blank" rel="noreferrer">
+          GitHub
+        </a>{" "}
+        |{" "}
+        <a href="https://webpages.charlotte.edu/malharaz/" target="_blank" rel="noreferrer">
+          CLT Web
+        </a>{" "}
+        |{" "}
+        <a href="https://mayaralharazi.github.io/" target="_blank" rel="noreferrer">
+          GitHub.io
+        </a>{" "}
+        |{" "}
+        <a href="https://itis3135-react-phi.vercel.app/" target="_blank" rel="noreferrer">
+          ITIS3135@Vercel
+        </a>{" "}
+        |{" "}
+        <a href="https://www.freecodecamp.org/mayaralharazi" target="_blank" rel="noreferrer">
+          freeCodeCamp
+        </a>{" "}
+        |{" "}
+        <a href="https://www.linkedin.com/in/mayar-alharazi-64a385241/" target="_blank" rel="noreferrer">
+          LinkedIn
+        </a>{" "}
+        |{" "}
+        <a href="https://validator.w3.org/" target="_blank" rel="noreferrer">
+          HTML Validator
+        </a>{" "}
+        |{" "}
+        <a href="https://jigsaw.w3.org/css-validator/" target="_blank" rel="noreferrer">
+          CSS Validator
+        </a>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
