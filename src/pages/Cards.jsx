2c3fe5628cbb0df9f860e5418727f7ed373c8e8b
mayaralharazi <malharaz@charlotte.edu>
2026-04-21 21:15:11 -0400

    Update Cards.jsx

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,9 +1,15 @@
+import PageTitle from "../components/PageTitle";
+
 function Cards() {
-    return (
+  return (
+    <>
+      <PageTitle title="Mayar Alharazi's Jolly Dolphin ~ ITIS 3135 ~ Cards" />
+
       <main>
         <h2>Cards</h2>
       </main>
-    );
-  }
-  
-  export default Cards;
+    </>
+  );
+}
+
+export default Cards;
